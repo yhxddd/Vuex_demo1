3bdd693384b0088d37ae9170c0b566f09eb7de56
yhxddd <872691624@qq.com>
2020-07-31 16:20:53 +0800

    初始化计数器项目的基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <my-add></my-add>
+    <p>-------------------------------</p>
+    <my-sub></my-sub>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// eslint-disable-next-line no-unused-vars
+import Add from './components/Add.vue'
+// eslint-disable-next-line no-unused-vars
+import Sub from './components/Sub.vue'
 
 export default {
-  name: 'App',
+  data () {
+    return {}
+  },
   components: {
-    HelloWorld
+    // eslint-disable-next-line vue/no-unused-components
+    'my-add': Add,
+    // eslint-disable-next-line vue/no-unused-components
+    'my-sub': Sub
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
